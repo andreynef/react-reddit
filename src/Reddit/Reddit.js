dc1fb1f9c362cda5c8f36809562a2dcc21e9a09c
Nefedyev Andrey <andreynef@yandex.ru>
2020-12-27 16:01:20 +0400

    update client id env

--- src/Reddit/Reddit.js
+++ src/Reddit/Reddit.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const REDIRECT_URI = 'http://localhost:3000/auth';
+// const REDIRECT_URI = 'http://localhost:3000/auth';//вынесен в env
 const DURATION = 'permanent'; //or 'temporary' for logging out every 1h.
 const SCOPE = 'read submit identity';
 const TYPE = 'code';
 // const SECRETold = 'tmfKWhwSI8JQfEcXqJgu3PNXLXo11w';//мой личный
-const SECRET = 'RD2BDYxwYPnDsixEt7GmfPQbXutB5Q';//2й аккаунт https://www.reddit.com/prefs/apps
+// const SECRET = 'RD2BDYxwYPnDsixEt7GmfPQbXutB5Q';//2й аккаунт https://www.reddit.com/prefs/apps
 
 //используется в компоненте с кликом для перехода на странуцу Reddit авторизации.
 export function getRedditAuthUrl () {
-  return `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=${TYPE}&state=random_string&redirect_uri=${REDIRECT_URI}&duration=${DURATION}&scope=${SCOPE}`
+  return `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=${TYPE}&state=random_string&redirect_uri=${process.env.REDIRECT_URI}&duration=${DURATION}&scope=${SCOPE}`
 }
 
 //используется в server.js для запроса в Reddit на получение access_token.
 export function getAccessTokenRequest(req) {
   return axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.REDIRECT_URI}`,
     {//подробности настройки в доке axios (github.com/axios/axios)
       auth: {
         username:process.env.CLIENT_ID,
-        password:SECRET
+        password:process.env.SECRET
       },
       headers: {'Content-type':'application/x-www-form-urlencoded'}//указ тип дабы не было ошибок
     }
