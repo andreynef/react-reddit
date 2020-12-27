560c3f0e78b09114fa33356eaa95cc5918e6a84a
Nefedyev Andrey <andreynef@yandex.ru>
2020-12-27 16:38:01 +0400

    update client id env

--- src/Reddit/Reddit.js
+++ src/Reddit/Reddit.js
@@ -11,18 +11,18 @@ const SECRET_PROD='dFDQOPkRwOhJML4YTDKG5v7vDJb0rg';//вынести в env не 
 const REDIRECT_URI_PROD='https://react-reddit-app-andrey.herokuapp.com/auth';//вынести в env не полся.
 //используется в компоненте с кликом для перехода на странуцу Reddit авторизации.
 export function getRedditAuthUrl () {
-  return `https://www.reddit.com/api/v1/authorize?client_id=${process.env.NODE_ENV='development' ? process.env.CLIENT_ID: CLIENT_ID_PROD}&response_type=${TYPE}&state=random_string&redirect_uri=${process.env.NODE_ENV='development' ? process.env.REDIRECT_URI : REDIRECT_URI_PROD}&duration=${DURATION}&scope=${SCOPE}`
+  return `https://www.reddit.com/api/v1/authorize?client_id=${process.env.NODE_ENV ? process.env.CLIENT_ID: CLIENT_ID_PROD}&response_type=${TYPE}&state=random_string&redirect_uri=${process.env.NODE_ENV ? process.env.REDIRECT_URI : REDIRECT_URI_PROD}&duration=${DURATION}&scope=${SCOPE}`
 }
 
 //используется в server.js для запроса в Reddit на получение access_token..
 export function getAccessTokenRequest(req) {
   return axios.post(
     'https://www.reddit.com/api/v1/access_token',
-    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.NODE_ENV='development' ? process.env.REDIRECT_URI : REDIRECT_URI_PROD}`,
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.NODE_ENV ? process.env.REDIRECT_URI : REDIRECT_URI_PROD}`,
     {//подробности настройки в доке axios (github.com/axios/axios)
       auth: {
-        username:process.env.NODE_ENV='development' ? process.env.CLIENT_ID : CLIENT_ID_PROD,
-        password:process.env.NODE_ENV='development' ? process.env.SECRET: SECRET_PROD,
+        username:process.env.NODE_ENV ? process.env.CLIENT_ID : CLIENT_ID_PROD,
+        password:process.env.NODE_ENV ? process.env.SECRET: SECRET_PROD,
       },
       headers: {'Content-type':'application/x-www-form-urlencoded'}//указ тип дабы не было ошибок
     }
